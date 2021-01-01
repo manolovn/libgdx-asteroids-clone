f88e71a62867ace9fa565d673cb77591c1529e4b
manolovn <manolo.vn@gmail.com>
2021-01-01 16:19:22 +0100

    config android launcher

--- android/src/dev/manolovn/burger/AndroidLauncher.java
+++ android/src/dev/manolovn/burger/AndroidLauncher.java
@@ -4,13 +4,15 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import dev.manolovn.burger.BurgerMenuGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		config.useGyroscope = false;
 		initialize(new BurgerMenuGame(), config);
 	}
 }
