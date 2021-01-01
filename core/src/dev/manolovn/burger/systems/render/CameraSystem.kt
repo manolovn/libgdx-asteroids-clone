48043bbefe2b0e611611b07d6e3ef2d58629c41a
manolovn <manolo.vn@gmail.com>
2021-01-01 16:22:47 +0100

    updated screen dimensions

--- core/src/dev/manolovn/burger/systems/render/CameraSystem.kt
+++ core/src/dev/manolovn/burger/systems/render/CameraSystem.kt
@@ -2,19 +2,20 @@ package dev.manolovn.burger.systems.render
 
 import com.artemis.EntitySystem
 import com.artemis.annotations.All
-import com.badlogic.gdx.Gdx.graphics
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import dev.manolovn.burger.BurgerMenuGame.Companion.H
+import dev.manolovn.burger.BurgerMenuGame.Companion.W
 
 @All
 class CameraSystem(
     private val batch: SpriteBatch
 ) : EntitySystem() {
 
-    private var camera = OrthographicCamera(graphics.width.toFloat(), graphics.height.toFloat())
+    private var camera = OrthographicCamera()
 
     override fun initialize() {
-        camera.setToOrtho(false, graphics.width.toFloat(), graphics.height.toFloat())
+        camera.setToOrtho(false, W, H)
         batch.projectionMatrix = camera.combined
     }
 
--- desktop/src/dev/manolovn/burger/desktop/DesktopLauncher.java
+++ desktop/src/dev/manolovn/burger/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import dev.manolovn.burger.BurgerMenuGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 740;
+		config.width = 800;
 		config.height = 480;
 		new LwjglApplication(new BurgerMenuGame(), config);
 	}
