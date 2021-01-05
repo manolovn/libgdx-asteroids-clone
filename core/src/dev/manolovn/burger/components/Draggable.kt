cc36225a0821ffd22d1cc361ea027155b094ba19
manolovn <manolo.vn@gmail.com>
2021-01-05 17:40:19 +0100

    removed unused componentes

--- core/src/dev/manolovn/burger/components/Draggable.kt
+++
@@ -1,9 +0,0 @@
-package dev.manolovn.burger.components
-
-import com.artemis.Component
-import com.badlogic.gdx.math.Vector2
-
-data class Draggable(
-        var downPoint: Vector2 = Vector2(0f, 0f),
-        var upPoint: Vector2 = Vector2(0f, 0f)
-): Component()
